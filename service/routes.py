9ea24df08f3ba6d02a1859b1055f02cf363a5eaf
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-09-22 16:58:55 -0700

    update flake

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,8 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -80,6 +82,8 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_accounts(account_id):
     """
@@ -98,6 +102,8 @@ def read_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -119,6 +125,8 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
