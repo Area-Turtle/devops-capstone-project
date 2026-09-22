2e73753761da6ab6b4b43eca85250a394fb3e0ae
Zach <60633052+Area-Turtle@users.noreply.github.com>
2026-09-22 15:29:54 -0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -125,6 +125,7 @@ class TestAccountService(TestCase):
 
     # ADD YOUR TEST CASES HERE ...
 
+    # READ TESTS
     def test_read_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
@@ -139,3 +140,12 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    # LIST TESTS
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
